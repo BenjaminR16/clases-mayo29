f2a23ffb32e1cc0910709b4bc26e179deebb5624
Benjamin <brcj16@gmail.com>
2026-05-29 10:34:22 +0200

    herencia, polimorfismo y callback

--- clases.js
+++ clases.js
@@ -4,6 +4,76 @@ export class clases {
     // es quien recibe los parametros que se envian a la clase
     constructor(nombre) {
         this.nombre = nombre
-        console.log("HOla ", nombre);
+        console.log("Hola ", nombre);
+    }
+
+    despedida(nombre) {
+        console.log(`adios${nombre}`)
     }
 }
+
+//herencia
+class padre {
+    casa() {
+        console.log("tengo una casa")
+    }
+}
+
+//herencia con super: es una manera de usar las variables del padre desde el hijo
+export class superPadre {
+    nombre = "pepito"
+
+}
+
+export class superHijo extends superPadre {
+    constructor(nombre) {
+        super(nombre)
+    }
+    metodo() {
+        console.log("super hijo ", this.nombre)
+    }
+}
+
+
+//la clase hijo es una extencion del padre, lo cual sa acceso a los metodos del padre
+export class hijo extends padre {
+
+}
+
+// polimorfismo
+export class animal {
+    haceRuido() {
+        console.log("grune")
+    }
+}
+
+export class perro extends animal {
+    haceRuido() {
+        console.log("ladra")
+    }
+}
+
+export class gato extends animal {
+    haceRuido() {
+        console.log("maulla")
+    }
+}
+
+//callBack
+export class emisor {
+    imprimir() {
+        console.log("metodo del emisor")
+    }
+}
+
+export class receptor {
+    callback
+    constructor(callback) {
+        this.callback = callback
+    }
+
+    imprimirCallback() {
+        this.callback.imprimir()
+    }
+}
+
--- modulo.js
+++ modulo.js
@@ -1,3 +1,32 @@
-import { clases } from './clases.js'
+import { clases, hijo, animal, gato, perro, superHijo, emisor, receptor } from './clases.js'
 
 const consola = new clases("Benjamin")
+consola.despedida("Benjamin")
+
+const herederoHijo = new hijo()
+
+console.log("------------------------------------------- ");
+//herencia: la clase hijo tiene acceso a los metodos del padre pero no los cambia
+herederoHijo.casa()
+
+
+//herencia con super
+const hijito = new superHijo()
+hijito.metodo()
+
+console.log("------------------------------------------- ");
+//polimorfismo: aunque las clases hijas compartan el mismo metodo, pueden actuar de manera distinta 
+const animales = new animal()
+const perrito = new perro()
+const michi = new gato()
+
+//esto se hace cuando queremos ejecutar varias clases de un solo ciclo, basicamente se le da el mismo nombre a todas y con un array recorremos 
+animales.haceRuido()
+perrito.haceRuido()
+michi.haceRuido()
+
+console.log("------------------------------------------- ");
+//callBack
+const recibirC = new receptor(new emisor)
+
+recibirC.imprimirCallback()
